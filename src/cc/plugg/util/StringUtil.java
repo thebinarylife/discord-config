b41acd2e58bdbc5d14dad20547e06df77196b1f9
zachary.baugh <zachary.baugh@Loaner4.bic.local>
2023-10-12 13:06:05 -0400

    Formatting

--- src/cc/plugg/util/StringUtil.java
+++ src/cc/plugg/util/StringUtil.java
@@ -13,15 +13,12 @@ public class StringUtil {
 	public static String STRINGS_MSGS_NAME_CONFIRMATION;
 	public static String STRINGS_MSGS_CATEGORY;
 	public static String STRINGS_MSGS_INSTAGRAM_ASK;
+	
 	public static String STRINGS_MSGS_EMAIL_CONFIRMATION;
 	public static String STRINGS_MSGS_EMAIL_VALID;
 	public static String STRINGS_MSGS_EMAIL_INVALID;
-	public static String STRINGS_MSGS_LOGIN_FINISHED;
 	
-	public static String STRINGS_MSGS_PREFIX_PLUGG;
-	public static String STRINGS_MSGS_PREFIX_ERROR;
-	public static String STRINGS_MSGS_PREFIX_SUCCESS;
-	public static String STRINGS_MSGS_PREFIX_VALIDATION;
+	public static String STRINGS_MSGS_LOGIN_FINISHED;
 	
 	private static final String emailRegexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
 	private static final String instagramRegexPattern = "^[\\w](?!.*?\\.{2})[\\w.]{1,28}[\\w]$";
