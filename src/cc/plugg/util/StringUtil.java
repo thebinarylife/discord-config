068c8c1ade9b079c78d53c4f0f46eee81da052e7
zachary.baugh <zachary.baugh@Loaner4.bic.local>
2023-10-12 16:59:04 -0400

    Variable Workup attached config

--- src/cc/plugg/util/StringUtil.java
+++ src/cc/plugg/util/StringUtil.java
@@ -1,5 +1,6 @@
 package cc.plugg.util;
 
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class StringUtil {
@@ -9,17 +10,33 @@ public class StringUtil {
 	public static String STRINGS_UTIL_PREFIXES_SUCCESS;
 	public static String STRINGS_UTIL_PREFIXES_VALIDATION;
 	
-	public static String STRINGS_MSGS_REGISTRATION_WELCOME;
-	public static String STRINGS_MSGS_REGISTRATION_NAME_CONFIRMATION;
+	public static String STRINGS_INDICATOR_EMAIL;
 	
-	public static String STRINGS_MSGS_REGISTRATION_EMAIL_CONFIRMATION;
-	public static String STRINGS_MSGS_REGISTRATION_EMAIL_VALID;
+	public static String STRINGS_MSGS_REGISTRATION_WELCOME;
+	
+	public static ArrayList<String> STRINGS_MSGS_REGISTRATION_NAME_CONFIRMATION;
+	
+	public static String STRINGS_MSGS_REGISTRATION_EMAIL_CONFIRM;
+	public static String STRINGS_MSGS_REGISTRATION_EMAIL_CONFIRMED;
+	public static String STRINGS_MSGS_REGISTRATION_EMAIL_SUBSCRIBE;
 	public static String STRINGS_MSGS_REGISTRATION_EMAIL_INVALID;
 	
-	public static String STRINGS_MSGS_REGISTRATION_CATEGORY;
 	public static String STRINGS_MSGS_REGISTRATION_INSTAGRAM_PROMPT;
+	public static String STRINGS_MSGS_REGISTRATION_INSTAGRAM_CONFIRMED;
 	
-	public static String STRINGS_MSGS_LOGIN_FINISHED;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_MSG;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_FRIEND_DESCRIPTION;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_FRIEND_EMOJI;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_PRODUCER_DESCRIPTION;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_PRODUCER_EMOJI;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_ARTIST_DESCRIPTION;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_ARTIST_EMOJI;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_AR_DESCRIPTION;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_AR_EMOJI;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_GA_DESCRIPTION;
+	public static String STRINGS_MSGS_REGISTRATION_CATEGORY_GA_EMOJI;
+	
+	public static String STRINGS_MSGS_REGISTRATION_LOGIN_FINISHED;
 	
 	private static final String emailRegexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
 	private static final String instagramRegexPattern = "^[\\w](?!.*?\\.{2})[\\w.]{1,28}[\\w]$";
